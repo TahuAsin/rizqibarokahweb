b0e02a031b306143362d4dfaee17e39d745acd09
Alifudin Nurhafidzi <nurhafidzialifudin@gmail.com>
2026-06-16 14:56:42 +0700

    feat: initialize project structure and implement core product, category, and contact management features

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->redirectGuestsTo(fn (Request $request) => route('admin.login'));
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
